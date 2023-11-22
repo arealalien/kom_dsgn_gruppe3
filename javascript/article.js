fd8e9ee72c0e45f29df9d3134fd3b86c78797d1c
dlirA01 ♡ <arild.edvin.andreassen@gmail.com>
2023-11-22 20:50:44 +0100

    made page transitions more smooth

--- javascript/article.js
+++ javascript/article.js
@@ -1,5 +1,6 @@
 const toIndexInner = document.querySelectorAll(".to-index .navbar-inner")[0];
 const toIndex = document.querySelectorAll(".to-index")[0];
+const pageOverlay = document.querySelectorAll(".page-overlay")[0];
 const navbarLink = document.querySelectorAll(".navbar-inner-logo-link")[0];
 const navbarAnimationDuration = ".35s";
 const navbarAnimationDetails = "cubic-bezier(.175, .685, .32, 1) forwards";
@@ -10,6 +11,7 @@ navbarLink.addEventListener("click", function(event) {
     toIndexInner.style.animation = 'none';
     toIndexInner.style.animation = `navbarInOut ${navbarAnimationDuration} ${navbarAnimationDetails}`;
     toIndex.style.animation = `navbarTopInOut ${navbarAnimationDuration} ${navbarAnimationDetails}`;
+    pageOverlay.style.animation = `pageOverlay ${navbarAnimationDuration} ${navbarAnimationDetails}`;
 
     setTimeout(() => {
         window.location.href = navbarLink.getAttribute("href");
